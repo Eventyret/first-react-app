51a1ffa53198349994f271214a425913403b40fa
Simen Daehlin <simen.dehlin@gmail.com>
2017-08-03 21:45:17 +0100

    Added a AddProject and a Handler to add categories and bind them

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
-import Projects from './components/projects'
+import Projects from './components/Projects';
+import AddProject from './components/AddProject';
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      projects: [
-        {
-          title: 'Business Website',
-          category: 'Web Design'
-        },
-        {
-          title: 'Social App',
-          category: 'Mobile Development'
-        },
-        {
-          title: 'Ecommerce Shopping Cart',
-          category: 'Web Development'
-        }
-      ]
+      projects: []
     }
   }
 
+  componentWillMount(){
+    this.setState({projects: [
+      {
+        title: 'Business Website',
+        category: 'Web Design'
+      },
+      {
+        title: 'Social App',
+        category: 'Mobile Development'
+      },
+      {
+        title: 'Ecommerce Shopping Cart',
+        category: 'Web Development'
+      }
+    ]});
+
+  } 
+  handleAddProject(project){
+    let projects = this.state.projects;
+    projects.push(project);
+    this.setState({projects:projects})
+  }
   render() {
     return (
       <div className="App">
-          My App
+          <AddProject addProject={this.handleAddProject.bind(this)} /> 
           <Projects projects={this.state.projects}/>
       </div>
     );
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import ProjectItem from './ProjectItem';
 
 class Projects extends Component {
+  
   render() {
-    console.log(this.props)
+    // eslint-disable-next-line
+    let projectItems;
+    if(this.props.projects){
+      // eslint-disable-next-line
+      projectItems = this.props.projects.map(project => {
+      return (
+        <ProjectItem key={project.title} project={project} />
+      );
+      });
+    }
     return (
       <div className="Projects">
-          My Projects
+          {projectItems}
       </div>
     );
   }

7d258a5e614f01805eb66f335181844092832e91
Simen Daehlin <simen.dehlin@gmail.com>
2017-08-03 19:32:51 +0100

    Added Projects

---
+++ src/components/projects.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Projects extends Component {
+  render() {
+    return (
+      <div className="Projects">
+          My Projects
+      </div>
+    );
+  }
+}
+
+export default Projects;

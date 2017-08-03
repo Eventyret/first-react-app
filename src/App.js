63a8e787ecc69455b577e942274b19596b7cbad1
Simen Daehlin <simen.dehlin@gmail.com>
2017-08-03 19:36:36 +0100

    Added first Array

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
 import Projects from './components/projects'
+
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      projects: [
+        {
+          title: 'Business Website',
+          category: 'Web Design'
+        },
+        {
+          title: 'Social App',
+          category: 'Mobile Development'
+        },
+        {
+          title: 'Ecommerce Shopping Cart',
+          category: 'Web Development'
+        }
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="App">
           My App
-          <Projects />
+          <Projects projects={this.state.projects}/>
       </div>
     );
   }
--- src/components/projects.js
+++ src/components/projects.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 class Projects extends Component {
   render() {
+    console.log(this.props)
     return (
       <div className="Projects">
           My Projects
